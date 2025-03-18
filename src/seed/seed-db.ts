7a5b49f08bcac1dbfc419cc68e08c7b4ba5215d1
GalileoAleman <116136971+GalileoAleman@users.noreply.github.com>
2025-03-17 22:21:25 -0400

    Seed error
    Seed error.

--- src/seed/seed-db.ts
+++ src/seed/seed-db.ts
@@ -9,12 +9,12 @@ async function main(){
     await prisma.orderAddress.deleteMany();
     await prisma.orderProduct.deleteMany();
     await prisma.order.deleteMany();
+    await prisma.userAddress.deleteMany();
     await prisma.user.deleteMany();
     await prisma.productImage.deleteMany();
     await prisma.product.deleteMany();
     await prisma.category.deleteMany();
     await prisma.country.deleteMany();
-    await prisma.userAddress.deleteMany();
 
     //Se usa los datos de prebas de la semilla de productos Geeks
     const {categories, products, users} = initialData
